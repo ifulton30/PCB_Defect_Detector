ba5b789a15dcd5b9fbb1ebf3d4ccbce84c60928d
smoothluvin <75822011+smoothluvin@users.noreply.github.com>
2025-07-12 14:13:16 -0700

    Setting up camera initialization using MSMF camera API backend on openCV. Next step is to complete apply_camera_settings from webcam_capture.py

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ This section will contain all the metrics and demo images from learning over the
 
 # References
 This section will list all the machine learning/computer vision techniques used along with the research papers studied to proceed with this project
+
+Webcam Capture API Backends:
+https://docs.opencv.org/3.4/d4/d15/group__videoio__flags__base.html#gga023786be1ee68a9105bf2e48c700294dab6ac3effa04f41ed5470375c85a23504
+
+List of all available VideoCapture properties that we can adjust:
+https://docs.opencv.org/4.x/d4/d15/group__videoio__flags__base.html#gaeb8dd9c89c10a5c63c139bf7c4f5704d
--- utils/webcam_capture.py
+++ utils/webcam_capture.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+
 
 class WebcamPCBCapture:
     """Webcam capture system optimized for PCB Imaging"""
@@ -9,6 +11,7 @@ class WebcamPCBCapture:
         self.cap = None
         self.is_initialized = False
 
+        # I will be utilizing these camera settings to adjust for optimal PCB inspection settings
         self.camera_settings = {
             'brightness': 128, # Middle range
             'contrast': 130,
@@ -20,16 +23,57 @@ class WebcamPCBCapture:
             'sharpness': 130
         }
 
+        # Calls initialize_camera to start up the webcam, but does not show frames yet
         self.initialize_camera()
 
     def initialize_camera(self):
-        """Initializing camera for PCB imaging"""
+        """Initializing camera for PCB imaging using MSMF backend"""
         try:
-            self.cap = cv2.VideoCapture(self.camera_id, cv2.CAP_DSHOW)
+            # This opens the camera while enforcing the MSMF backend which is a Capture API backend.
+            self.cap = cv2.VideoCapture(self.camera_id, cv2.CAP_MSMF)
 
+            # Fallback to default Capture API backend 
             if not self.cap.isOpened():
                 self.cap = cv2.VideoCapture(self.camera_id)
+
+            # Raising an error if neither backends can be used and camera can't be opened
+            if not self.cap.isOpened():
+                raise Exception(f"Cannot open camera {self.camera_id}")
+            
+            # Setting the resolution of the camera. The Logitech C920 shoots up to 1920x1080
+            self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.resolution[0])
+            self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.resolution[1])
+            
+            # Setting the camera settings defined when instantiating the WebcamPCBCapture object
+            self.apply_camera_settings()
+
+            # Successful initialization message which also prints the actual resolution of camera at startup
+            self.is_initialized = True
+            print(f"Camera initialized successfully at resolution: {self.get_actual_resolution}")
 
         except Exception as e:
             print(f"Error initializing camera: {e}")
             self.is_initialized = False
+
+    def apply_camera_settings(self):
+        """
+        This function is used to apply the camera settings which were defined in 
+        the constructor and initialize_camera functions
+        """
+
+        # Checking to make sure that the camera is open
+        if not self.cap:
+            print("Camera is not initialized so we cannot apply camera settings")
+            return
+
+class PCBTestingSession:
+    """Testing manager for PCB Defect Detection"""
+
+    def __init__(self, model, config, output_dir="test_results"):
+        self.model = model
+        self.config = config
+        self.output_dir = Path(output_dir)
+        self.output_dir.mkdir(exist_ok = True)
+
+    # Initializing the webcam by instantiating the WebcamPCBCapture class
+    self.webcam = WebcamPCBCapture()
